0432c83079cdaf653a34763a159d77e2bd08a1ea
Swayamsiddha <swayamsiddharath@gmail.com>
2021-03-30 21:14:18 +0530

    Initial commit

---
+++ src/app/services/quiz.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class QuizService {
+
+  constructor(private http: HttpClient) { }
+
+  get(url: string) {
+    return this.http.get(url);
+  }
+
+  getAll() {
+    return [
+      { id: 'data/javascript.json', name: 'JavaScript' },
+      { id: 'data/sql.json', name: 'SQL' },
+      { id: 'data/htmlcss.json', name: 'HTML & CSS' },
+      { id: 'data/python.json', name: 'Python' }
+    ];
+  }
+
+}
